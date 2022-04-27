f90b092c15107eb5a92c4fc6d5af307f796cdf93
Tomas Dittmann <chaosmeist3r@gmail.com>
2022-04-27 18:11:59 +0200

    restrict width as well

--- RCB/Assets/js/function.js
+++ RCB/Assets/js/function.js
@@ -5,5 +5,9 @@ KB.on('dom.ready', function() {
         if (item.scrollHeight > 200) {
             item.style.height = "200px"
         }
+        
+        if (item.scrollWidth > 500) {
+            item.style.width = "500px"
+        }
     }
 });
