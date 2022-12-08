47030b4037d2111f550c0889a6556720a078835b
Tomas Dittmann <chaosmeist3r@gmail.com>
2022-12-08 14:28:05 +0100

    Update function.js

--- RCB/Assets/js/function.js
+++ RCB/Assets/js/function.js
@@ -5,9 +5,5 @@ KB.on('dom.ready', function() {
         if (item.scrollHeight > 200) {
             item.style.height = "200px"
         }
-        
-        if (item.scrollWidth > 1000) {
-            item.style.width = "1000px"
-        }
     }
 });
