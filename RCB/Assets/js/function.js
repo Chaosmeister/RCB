7e5201226f2d64f06acc985c58f85aeafc0a26b2
Tomas Dittmann <chaosmeist3r@gmail.com>
2023-09-06 13:53:53 +0200

    Update function.js

--- RCB/Assets/js/function.js
+++ RCB/Assets/js/function.js
@@ -1,9 +1,34 @@
-KB.on('dom.ready', function() {
-    var list = [...document.getElementsByTagName("pre")]
+const copyButtonLabel = "Copy";
 
-    for (let item of list) {
+async function copyCode(code, button) {
+    let text = code.innerText;
+
+    await navigator.clipboard.writeText(text);
+    
+    // visual feedback that task is completed
+    button.innerText = "Code copied";
+
+    setTimeout(() => {
+        button.innerText = copyButtonLabel;
+    }, 700);
+  }
+
+KB.on('dom.ready', function () {
+    for (let item of [...document.getElementsByTagName("pre")]) {
         if (item.scrollHeight > 200) {
             item.style.height = "200px"
+        }
+    }
+
+    if (navigator.clipboard) {
+        for (let item of [...document.getElementsByTagName("code")]) {
+            let button = document.createElement("button");
+
+            button.innerText = copyButtonLabel;
+            item.prepend(button);
+            button.addEventListener("click", async () => {
+                await copyCode(item, button);
+            });
         }
     }
 });
