a18c91740d1345f52b497c5f6560dce735fd80ed
Tomas Dittmann <chaosmeist3r@gmail.com>
2023-09-29 12:13:02 +0200

    only add buttons for non-highlighted code blocks

--- Assets/js/function.js
+++ Assets/js/function.js
@@ -4,14 +4,14 @@ async function copyCode(code, button) {
     let text = code.innerText.substring(button.innerText.length);
 
     await navigator.clipboard.writeText(text);
-    
+
     // visual feedback that task is completed
     button.innerText = "Code copied";
 
     setTimeout(() => {
         button.innerText = copyButtonLabel;
     }, 700);
-  }
+}
 
 KB.on('dom.ready', function () {
     for (let pre of [...document.getElementsByTagName("pre")]) {
@@ -21,13 +21,15 @@ KB.on('dom.ready', function () {
 
         if (navigator.clipboard) {
             for (let code of [...pre.getElementsByTagName("code")]) {
-                let button = document.createElement("button");
-    
-                button.innerText = copyButtonLabel;
-                code.append(button);
-                button.addEventListener("click", async () => {
-                    await copyCode(code, button);
-                });
+                if (code.classList.length == 0) { // only add the button if no external syntaxhighlighting will be used
+                    let button = document.createElement("button");
+
+                    button.innerText = copyButtonLabel;
+                    code.append(button);
+                    button.addEventListener("click", async () => {
+                        await copyCode(code, button);
+                    });
+                }
             }
         }
     }
