2e336f183f01c63e5c293cf6d768f3be80221502
Han Solo <hansolo@zotac.fritz.box>
2024-10-25 10:28:24 +0200

    add de lang files

---
+++ de_DE/translations.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'Make codeblocks resizable, scrollable and limit their initial height to 200 pixels' => 'Codeblocks in der Größe veränderbar und scrollbar machen und ihre Anfangshöhe auf 200 Pixel begrenzen',
+);
